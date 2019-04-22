4984dfa446d1706befe80b71f75e33cbcea68d25
Jordan Rivera <jordan.rivera34@gmail.com>
2019-04-22 14:47:37 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(function (tut) {
+    tut = tut.split(' ');
+    for (var i = 0; i < tut.length; i++) {
+      tut[i] = tut[i].charAt(0).toUpperCase() + tut[i].slice(1);
+    }
+    return tut.join(' ');
+  });
 }
